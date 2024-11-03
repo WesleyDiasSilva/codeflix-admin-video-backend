a381569e08a1597e9de298b35d6429ec9fa852d7
Wesley Dias <wesley_diaz8@outlook.com>
2024-11-03 16:14:18 +0000

    adicionar testes para repository de category em memory com search

--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,10 +1,22 @@
+import { SortDirection } from "../../../shared/domain/repository/search-params";
 import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
+import { InMemorySearchableRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../domain/category.entity";
 import { CategoryRepository } from "../../domain/category.repository";
 
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> implements CategoryRepository {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> implements CategoryRepository {
+  sortableFields: string[] = ['name', 'created_at'];
+  protected async applyFilter(entities: Category[], filter: string): Promise<Category[]> {
+    if (!filter) {
+      return entities;
+    }
+    return entities.filter(entity => entity.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  protected applySort(entities: Category[], sort: string | null, sort_dir: SortDirection | null, custom_getter?: (sort: string, entity: Category) => any): Category[] {
+    return sort ? super.applySort(entities, sort, sort_dir, custom_getter) : super.applySort(entities, 'created_at', 'desc');
+  }
   getEntity(): new (...args: any[]) => Category {
     return Category;
   }
