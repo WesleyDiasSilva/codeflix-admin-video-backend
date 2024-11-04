ed48f7f04f5dc64e26b43f9b8fc42df5009401af
Wesley Dias <wesley_diaz8@outlook.com>
2024-11-04 23:01:04 +0000

    adicionar caso de uso de listagem de categorias

---
+++ src/category/application/use-cases/list-category.use-case.ts
@@ -0,0 +1,31 @@
+import { PaginationOutput, PaginationOutputMapper } from "../../../shared/application/pagination-output";
+import { IUseCase } from "../../../shared/application/use-case.interface";
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import { CategoryFilter, CategorySearchParams, CategorySearchResult, ICategoryRepository } from "../../domain/category.repository";
+import { CategoryOutput, CategoryOutputMapper } from "./common/category-output";
+
+export class ListCategoriesUseCase implements IUseCase<ListCategoriesInput, ListCategoriesOutput>{
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+  async execute(input: ListCategoriesInput): Promise<ListCategoriesOutput> {
+    const params = new CategorySearchParams(input);
+    const searchResult = await this.categoryRepository.search(params);
+    return this.toOutput(searchResult);
+  }
+
+  private toOutput(searchResult: CategorySearchResult): ListCategoriesOutput {
+    const outputItems = searchResult.items.map((category) => CategoryOutputMapper.toOutput(category));
+    return PaginationOutputMapper.toOutput(outputItems, searchResult)
+  }
+}
+
+
+export type ListCategoriesInput = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  sort_dir?: SortDirection | null;
+  filter?: CategoryFilter | null;
+}
+
+export type ListCategoriesOutput = PaginationOutput<CategoryOutput>;
