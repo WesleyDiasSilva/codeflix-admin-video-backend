4ca8323ba6a0dda73281bff76b8c603767a0be41
Wesley Dias <wesley_diaz8@outlook.com>
2024-11-07 22:52:15 +0000

    remover log do filter de entity validation

--- src/nest-modules/shared/filters/entity-validation/entity-validation.filter.ts
+++ src/nest-modules/shared/filters/entity-validation/entity-validation.filter.ts
@@ -9,20 +9,22 @@ export class EntityValidationFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response: Response = ctx.getResponse();
 
-    console.log(exception);
+    console.log(exception.error);
 
     response.status(422).json({
       statusCode: 422,
       error: 'Unprocessable Entity',
-      message: union(
-        ...exception.error.reduce(
-          (acc, error) =>
-            acc.concat(
-              typeof error === 'string' ? [[error]] : Object.values(error),
+      message: Array.isArray(exception.error)
+        ? union(
+            ...exception.error.reduce(
+              (acc, error) =>
+                acc.concat(
+                  typeof error === 'string' ? [[error]] : Object.values(error),
+                ),
+              [],
             ),
-          [],
-        ),
-      ),
+          )
+        : ['An unexpected error occurred.'],
     });
   }
 }
