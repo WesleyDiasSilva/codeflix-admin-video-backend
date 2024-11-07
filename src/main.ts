e33976b39a953f0164351ce21d0c2dc268b29ad4
Wesley Dias <wesley_diaz8@outlook.com>
2024-11-07 21:17:38 +0000

    adicionar filters para erros de not found e entity validation da camada de dominio

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WrapperDataInterceptor } from './nest-modules/shared/interceptors/wrapper-data/wrapper-data.interceptor';
+import { NotFoundFilter } from './nest-modules/shared/filters/not-found/not-found.filter';
+import { EntityValidationFilter } from './nest-modules/shared/filters/entity-validation/entity-validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,8 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(new NotFoundFilter());
+  app.useGlobalFilters(new EntityValidationFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
