7e8c2bd5e5238533f2901e6c6dcb8ad960757f05
Wesley Dias <wesley_diaz8@outlook.com>
2024-11-08 14:42:31 +0000

    adicionar configuração para migrate

---
+++ src/migrate.ts
@@ -0,0 +1,13 @@
+import { migrator } from './core/shared/infra/db/sequelize/migrator';
+import { NestFactory } from '@nestjs/core';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { MigrationsModule } from './nest-modules/database/migrations.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(MigrationsModule, {
+    logger: ['error'],
+  });
+  const sequelize = app.get(getConnectionToken());
+  migrator(sequelize).runAsCLI();
+}
+bootstrap();
